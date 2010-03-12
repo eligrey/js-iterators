606f25d62d261d6ef0ee5733cda41ea53c7fe87c
Eli Grey <me@eligrey.com>
2010-03-11 23:53:01 -0500

    Replaced Array overriding hack with Array.fromIterable()

---
+++ array-from-iterable.js
@@ -0,0 +1,27 @@
+/*
+ * JavaScript Array.fromIterable iterable expander
+ * Requires JavaScript 1.7+
+ *
+ * 2010-03-11
+ * 
+ * By Elijah Grey, http://eligrey.com
+ *
+ * License: GNU GPL v3 and the X11/MIT license
+ *   See COPYING.md
+ */
+
+"use strict";
+
+Array.fromIterable = function (iterable, flag) {
+	var arr = [],
+	// Don't use new Iterator() as it disregards custom iterators
+	iter = Iterator(iterable, arguments.length < 2 ? true : flag);
+	
+	try {
+		while (true) {
+			arr.push(iter.next());
+		}
+	} catch (ex if ex instanceof StopIteration) {
+		return arr;
+	}
+};
